dd5f47cefd25931c171faa012c982df53a13fb86
Benjamin Sokolik <sokolik_b@hotmail.com>
2022-08-25 09:53:29 -0600

    updated query, fixed time errors

--- assets/main.js
+++ assets/main.js
@@ -1,26 +1,35 @@
 // Manage time elements and
 // Lay out variables
-let moment = moment();
+// let moment = moment();
 // console.log(moment.toString());
 var theDate = moment().format('LL');
-document.getElementById("momentDate").innerHTML = theDate;
+document.getElementById("momentDate").innerHTML = theDate; 
 
-
-const url = `https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid={APIKey}`;
-// *API KEY: 5c0c761d0687f69cd75f2132ebbdce74
+const url = `https://api.openweathermap.org/data/2.5/onecall?lat=59&lon=9905fc9a99b787e05e7e25f1ee7e0628`;
+// *API KEY: 9905fc9a99b787e05e7e25f1ee7e0628
 const APIKey = "4d8fb5b93d4af21d66a2948710284366";
 const form = document.querySelector(".searchArea form");
 const input = document.querySelector(".searchArea input");
 var inputValue = input.value;
 const list = document.querySelector(".cityButtons .cities");
+var button = document.querySelector(".button")
 
-//Prevent default behavior
+//Prevent default behavior, grab input
 function noDefault(event) {
     event.preventDefault();
+    const inputVal = input.value;
     console.log(event);
   }
     
-  inputValue.addEventListener("click", noDefault);
+  button.addEventListener("click", noDefault);
 
+
+//Fetch request for data
+
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    // do stuff with the data
+  })
     
-   
+  // https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=9905fc9a99b787e05e7e25f1ee7e0628
