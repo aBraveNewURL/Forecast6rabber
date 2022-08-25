d25862d742f64692c8a4fa0482bebcc1e02f49a8
Benjamin Sokolik <sokolik_b@hotmail.com>
2022-08-25 15:12:05 -0600

    updated JS and HTML

--- assets/main.js
+++ assets/main.js
@@ -1,37 +1,46 @@
-// Manage time elements and
-// Lay out variables
-// let moment = moment();
-// console.log(moment.toString());
+// Manage time elements
 var theDate = moment().format('LL');
 document.getElementById("momentDate").innerHTML = theDate;
 
-const url = `https://api.openweathermap.org/data/2.5/weather?&units=imperial&appid=9905fc9a99b787e05e7e25f1ee7e0628`;
-// *API KEY: 9905fc9a99b787e05e7e25f1ee7e0628
-const APIKey = "4d8fb5b93d4af21d66a2948710284366";
-const form = document.querySelector(".searchArea form");
-const input = document.querySelector(".searchArea input");
-var inputValue = input.value;
-const list = document.querySelector(".cityButtons .cities");
-var button = document.querySelector(".button")
 
-//Prevent default behavior, grab input
-function noDefault(event) {
-  event.preventDefault();
-  const inputVal = input.value;
-  console.log(event);
+// *API KEY: 4d8fb5b93d4af21d66a2948710284366
+
+const api= {
+  base: "https://api.openweathermap.org/data/2.5/",
+  key: "4d8fb5b93d4af21d66a2948710284366"
 }
 
-button.addEventListener("click", noDefault);
+const search = document.querySelector(".search");
+search.addEventListener("keypress", setQuery);
+
+function setQuery(evt) {
+  if (evt.keyCode == 13) {
+    evt.preventDefault();
+    getResults(search.value);
+    console.log(search.value);
+  }
+}
+
+function getResults (query) {
+  fetch(`${api.base}weather?q=${query}&units=imperial&APPID=${api.key}`)
+  .then(weather => {
+    return weather.json();
+  }) .then(displayResults); 
+};
+
+function displayResults (weather) {
+  console.log(weather);
+  cityMain.innertext = `${weather.name}, ${weather.main.temp}, ${weather.wind.speed}, ${weather.main.humidity}`; 
+  let temp = document.querySelector('.current .temp');
+  cityMain.innerHTML = `${weather.name}`;
+  iconMain.innerHTML = `${weather[0]["description"]}`;
+  tempMain.innerHTML = `Temp: ${Math.round(weather.main.temp)}<span> °F</span>`;
+  windMain.innerHTML = `Wind: ${weather.wind.speed}mph`;
+  humidMain.innerHTML = `Humidity: ${weather.main.humidity}%`;
+  // TODO uvIndexMain.innerHTML = `UV Index: ${}`;
+}
 
 
-//Fetch request for data
-
-// fetch(url)
-//   .then(response => response.json())
-// console.log(response)
-//   .then(data => {
-//     // do stuff with the data
-//   })
 
 
 
