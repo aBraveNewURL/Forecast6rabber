dcba12e11703b872986548ce2e037a58a7b2933b
Benjamin Sokolik <sokolik_b@hotmail.com>
2022-08-30 13:57:31 -0600

    working on icon

--- assets/main.js
+++ assets/main.js
@@ -2,13 +2,13 @@
 var theDate = moment().format('LL');
 document.getElementById("momentDate").innerHTML = theDate;
 
-
 // *API KEY: 4d8fb5b93d4af21d66a2948710284366
 
-const api= {
+let icon = document.querySelector(iconMain)
+const api = {
   base: "https://api.openweathermap.org/data/2.5/",
   key: "4d8fb5b93d4af21d66a2948710284366"
-}
+};
 
 const search = document.querySelector(".search");
 search.addEventListener("keypress", setQuery);
@@ -18,30 +18,50 @@ function setQuery(evt) {
     evt.preventDefault();
     getResults(search.value);
     console.log(search.value);
-  }
-}
 
-function getResults (query) {
-  fetch(`${api.base}weather?q=${query}&units=imperial&APPID=${api.key}`)
-  .then(weather => {
-    return weather.json();
-  }) .then(displayResults); 
+  }
 };
 
-function displayResults (weather) {
+function getUVI(query) {
+  fetch(`${api.base}uvi?lat=${lat}&lon=${lon}&appid=${api.key}`)
+    .then(weather => {
+      return weather.json();
+    }).then(displayResults);
+    console.log();
+};
+
+function getResults(query) {
+  fetch(`${api.base}weather?q=${query}&units=imperial&APPID=${api.key}`)
+    .then(weather => {
+      return weather.json();
+    }).then(displayResults);
+  function getUVI(query) {
+    fetch(`${api.base}uvi?lat=${lat}&lon=${lon}&appid=${api.key}`)
+      .then(weather => {
+        return weather.json();
+      }).then(displayResults);
+  };
+};
+
+function displayResults(weather) {
   console.log(weather);
-  cityMain.innertext = `${weather.name}, ${weather.main.temp}, ${weather.wind.speed}, ${weather.main.humidity}`; 
-  let temp = document.querySelector('.current .temp');
+  let lat = `${weather.coord.lat}`
+  let lon = `${weather.coord.lon}`
+  console.log("LAT:", lat);
+  console.log("LONG:", lon);
   cityMain.innerHTML = `${weather.name}`;
-  iconMain.innerHTML = `${weather[0]["description"]}`;
+  iconMain.innerHTML = `${weather.weather[1].main.icon}`;
   tempMain.innerHTML = `Temp: ${Math.round(weather.main.temp)}<span> °F</span>`;
   windMain.innerHTML = `Wind: ${weather.wind.speed}mph`;
   humidMain.innerHTML = `Humidity: ${weather.main.humidity}%`;
-  // TODO uvIndexMain.innerHTML = `UV Index: ${}`;
-}
+  };
 
 
+// https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key} <--------fetch
 
+//UVINDEX https://api.openweathermap.org/data/2.5/uvi?lat={lat}&lon={lon}&appid={API}
+
+// https://github.com/aBraveNewURL/Forecast6rabber
 
 
 
