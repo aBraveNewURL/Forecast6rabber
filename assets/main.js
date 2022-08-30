d2849a7bb39eabe873d8af61f60af6a9ab0bca49
Benjamin Sokolik <sokolik_b@hotmail.com>
2022-08-30 16:56:44 -0600

    updated icon

--- assets/main.js
+++ assets/main.js
@@ -4,7 +4,6 @@ document.getElementById("momentDate").innerHTML = theDate;
 
 // *API KEY: 4d8fb5b93d4af21d66a2948710284366
 
-let icon = document.querySelector(iconMain)
 const api = {
   base: "https://api.openweathermap.org/data/2.5/",
   key: "4d8fb5b93d4af21d66a2948710284366"
@@ -17,17 +16,25 @@ function setQuery(evt) {
   if (evt.keyCode == 13) {
     evt.preventDefault();
     getResults(search.value);
-    console.log(search.value);
-
+    console.log(search.value)
+    function getUVIndex() {
+      
+      $.ajax({
+        type: 'GET',
+        dataType: 'json',
+        beforeSend: function(request) {
+          request.setRequestHeader('x-access-token', 'c8c1aa40c93056f8ac5648981881de2d');
+        },
+        url: 'https://api.openuv.io/api/v1/uv?lat=' + lat + '&lng=' + lon + '&alt=' + alt + '&ozone=' + ozone + '&dt=' + dt,
+        success: function(response) {
+          //handle successful response
+        },
+        error: function(response) {
+          // handle error response
+        }
+      });
+     };
   }
-};
-
-function getUVI(query) {
-  fetch(`${api.base}uvi?lat=${lat}&lon=${lon}&appid=${api.key}`)
-    .then(weather => {
-      return weather.json();
-    }).then(displayResults);
-    console.log();
 };
 
 function getResults(query) {
@@ -35,27 +42,32 @@ function getResults(query) {
     .then(weather => {
       return weather.json();
     }).then(displayResults);
-  function getUVI(query) {
-    fetch(`${api.base}uvi?lat=${lat}&lon=${lon}&appid=${api.key}`)
-      .then(weather => {
-        return weather.json();
-      }).then(displayResults);
-  };
 };
 
 function displayResults(weather) {
   console.log(weather);
   let lat = `${weather.coord.lat}`
   let lon = `${weather.coord.lon}`
+  var iconCode = `${weather.weather[0].icon}`;
+  var iconURL = "http://openweathermap.org/img/w/" + iconCode + ".png";
   console.log("LAT:", lat);
   console.log("LONG:", lon);
+  var city = `${weather.name}`;
+  var temp = `${Math.round(weather.main.temp)}`;
+  var wind = `${weather.wind.speed}`;
+  var humid = `${weather.main.humidity}`;
+  document.getElementById("cityMain").value = city;
   cityMain.innerHTML = `${weather.name}`;
-  iconMain.innerHTML = `${weather.weather[1].main.icon}`;
   tempMain.innerHTML = `Temp: ${Math.round(weather.main.temp)}<span> °F</span>`;
   windMain.innerHTML = `Wind: ${weather.wind.speed}mph`;
   humidMain.innerHTML = `Humidity: ${weather.main.humidity}%`;
+  document.getElementById("iconMain").src = iconURL;
   };
 
+
+
+
+  // let icon = document.querySelector(iconMain)
 
 // https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key} <--------fetch
 
