e7a56b88dab9ab8c566e1e81ba1e72eb9cd846c2
Benjamin Sokolik <sokolik_b@hotmail.com>
2022-10-09 18:33:52 -0600

    fixed 5 day API calls

--- assets/main.js
+++ assets/main.js
@@ -9,11 +9,6 @@ const api = {
   key: "4d8fb5b93d4af21d66a2948710284366"
 };
 
-// const api5 = {
-//   base: "https://api.openweathermap.org/data/2.5/forecast",
-//   key: "4d8fb5b93d4af21d66a2948710284366"
-// };
-
 const search = document.querySelector(".search");
 var uvBox = document.getElementById("uvBox");
 search.addEventListener("keypress", setQuery);
@@ -35,37 +30,88 @@ function getResults(query) {
     .then(displayResults);
 };
 
-// function getResults5(weather) {
-//   lat = `${weather.coord.lat}`;
-//   lon = `${weather.coord.lon}`;
-//   fetch(`${api.base}forecast?q=${query}&units=imperial&APPID=${api.key}`)
-//     .then(forecast => {
-//       console.log(forecast);
-//       return (forecast).json();
-//     })
-//     .then(displayResults(forecast));    
-// };
+
 
 function displayResults(weather) {
 
   console.log(weather);
-  // console.log(forecast);
-  var lat = `${city.coord.lat}`;
-  var lon = `${city.coord.lon}`;
-  var iconCode = `${list[0].weather[0].icon}`;
+  var lat = `${weather.city.coord.lat}`;
+  var lon = `${weather.city.coord.lon}`;
+  var iconCode = `${weather.list[0].weather[0].icon}`;
   var iconURL = "http://openweathermap.org/img/w/" + iconCode + ".png";
-  console.log("LAT:", lat);
-  console.log("LONG:", lon);
-  var city = `${weather.name}`;
-  var temp = `${Math.round(weather.main.temp)}`;
-  var wind = `${weather.wind.speed}`;
-  var humid = `${weather.main.humidity}`;
-  document.getElementById("cityMain").value = city;
-  cityMain.innerHTML = `${weather.name}`;
-  tempMain.innerHTML = `Temp: ${Math.round(weather.main.temp)}<span> °F</span>`;
-  windMain.innerHTML = `Wind: ${weather.wind.speed}mph`;
-  humidMain.innerHTML = `Humidity: ${weather.main.humidity}%`;
   document.getElementById("iconMain").src = iconURL;
+  var city = `${weather.city.name}`;
+  var temp = `${Math.round(weather.list[0].main.temp)}`;
+  var wind = `${weather.list[0].wind.speed}`;
+  var humid = `${weather.list[0].main.humidity}`;
+
+  document.getElementById("cityMain").value = city;
+  cityMain.innerHTML = `${weather.city.name}`;
+  tempMain.innerHTML = `Temp: ${Math.round(weather.list[0].main.temp)}<span> °F</span>`;
+  windMain.innerHTML = `Wind: ${weather.list[0].wind.speed}mph`;
+  humidMain.innerHTML = `Humidity: ${weather.list[0].main.humidity}%`;
+  
+
+  var date1 = moment().add(1, 'days').format('LL');
+  document.getElementById("date1").innerHTML = date1;
+  var iconD1 = `${weather.list[1].weather[0].icon}`;
+  var iconURLD1 = "http://openweathermap.org/img/w/" + iconD1 + ".png";
+  document.getElementById("icon1").src = iconURLD1;
+  var temp1 = `${Math.round(weather.list[1].main.temp)}`;
+  var wind1 = `${weather.list[1].wind.speed}`;
+  var humid1 = `${weather.list[1].main.humidity}`;
+  tempMain1.innerHTML = `Temp: ${Math.round(weather.list[1].main.temp)}<span> °F</span>`;
+  windMain1.innerHTML = `Wind: ${weather.list[1].wind.speed}mph`;
+  humidMain1.innerHTML = `Humidity: ${weather.list[1].main.humidity}%`;
+
+  var date2 = moment().add(2, 'days').format('LL');
+  document.getElementById("date2").innerHTML = date2;
+  var iconD2 = `${weather.list[2].weather[0].icon}`;
+  var iconURLD2 = "http://openweathermap.org/img/w/" + iconD2 + ".png";
+  document.getElementById("icon2").src = iconURLD2;
+  var temp2 = `${Math.round(weather.list[2].main.temp)}`;
+  var wind2 = `${weather.list[2].wind.speed}`;
+  var humid2 = `${weather.list[2].main.humidity}`;
+  tempMain2.innerHTML = `Temp: ${Math.round(weather.list[2].main.temp)}<span> °F</span>`;
+  windMain2.innerHTML = `Wind: ${weather.list[2].wind.speed}mph`;
+  humidMain2.innerHTML = `Humidity: ${weather.list[2].main.humidity}%`;
+  
+  var date3 = moment().add(3, 'days').format('LL');
+  document.getElementById("date3").innerHTML = date3;
+  var iconD3 = `${weather.list[3].weather[0].icon}`;
+  var iconURLD3 = "http://openweathermap.org/img/w/" + iconD3 + ".png";
+  document.getElementById("icon3").src = iconURLD3;
+  var temp3 = `${Math.round(weather.list[3].main.temp)}`;
+  var wind3 = `${weather.list[3].wind.speed}`;
+  var humid3 = `${weather.list[3].main.humidity}`;
+  tempMain3.innerHTML = `Temp: ${Math.round(weather.list[3].main.temp)}<span> °F</span>`;
+  windMain3.innerHTML = `Wind: ${weather.list[3].wind.speed}mph`;
+  humidMain3.innerHTML = `Humidity: ${weather.list[3].main.humidity}%`;
+
+  var date4 = moment().add(4, 'days').format('LL');
+  document.getElementById("date4").innerHTML = date4;
+  var iconD4 = `${weather.list[4].weather[0].icon}`;
+  var iconURLD4 = "http://openweathermap.org/img/w/" + iconD4 + ".png";
+  document.getElementById("icon4").src = iconURLD4;
+  var temp4 = `${Math.round(weather.list[4].main.temp)}`;
+  var wind4 = `${weather.list[4].wind.speed}`;
+  var humid4 = `${weather.list[4].main.humidity}`;
+  tempMain4.innerHTML = `Temp: ${Math.round(weather.list[4].main.temp)}<span> °F</span>`;
+  windMain4.innerHTML = `Wind: ${weather.list[4].wind.speed}mph`;
+  humidMain4.innerHTML = `Humidity: ${weather.list[4].main.humidity}%`;
+
+  var date5 = moment().add(5, 'days').format('LL');
+  document.getElementById("date5").innerHTML = date5;
+  var iconD5 = `${weather.list[5].weather[0].icon}`;
+  var iconURLD5 = "http://openweathermap.org/img/w/" + iconD5 + ".png";
+  document.getElementById("icon5").src = iconURLD5;
+  var temp5 = `${Math.round(weather.list[5].main.temp)}`;
+  var wind5 = `${weather.list[5].wind.speed}`;
+  var humid5 = `${weather.list[5].main.humidity}`;
+  tempMain5.innerHTML = `Temp: ${Math.round(weather.list[5].main.temp)}<span> °F</span>`;
+  windMain5.innerHTML = `Wind: ${weather.list[5].wind.speed}mph`;
+  humidMain5.innerHTML = `Humidity: ${weather.list[5].main.humidity}%`;
+
 
 
   function getUVIndex() {
@@ -107,19 +153,3 @@ function displayResults(weather) {
 
   getUVIndex();
 };
-
-
-
-
-
-
-
-
-
-// let icon = document.querySelector(iconMain)
-
-// https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key} <--------fetch
-
-//UVINDEX https://api.openweathermap.org/data/2.5/uvi?lat={lat}&lon={lon}&appid={API}
-
-// https://github.com/aBraveNewURL/Forecast6rabber
